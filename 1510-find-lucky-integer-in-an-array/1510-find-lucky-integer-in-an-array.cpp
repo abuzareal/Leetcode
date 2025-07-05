cc0513cada7cd983d77d456613fb2ded64da46bf
Abuzar Tamboli <abuzart1999@gmail.com>
2025-07-05 23:46:02 +0530

    Time: 0 ms (100%), Space: 14.1 MB (5.42%) - LeetHub

--- 1510-find-lucky-integer-in-an-array/1510-find-lucky-integer-in-an-array.cpp
+++ 1510-find-lucky-integer-in-an-array/1510-find-lucky-integer-in-an-array.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int findLucky(vector<int>& arr) {
-        int freq[501]={0}, xMax=0;
-        for(int x: arr){
+        int freq[501] = {0}, xMax = 0;
+        for (int x : arr) {
             freq[x]++;
-            xMax=max(x, xMax);
+            xMax = max(x, xMax);
         }
-        for(int x=xMax; x>=1; x--){
-            if (freq[x]==x) return x;
+        for (int x = xMax; x >= 1; x--) {
+            if (freq[x] == x)
+                return x;
         }
         return -1;
     }
